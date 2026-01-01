4f2e06d3a687d9c94e941b4ae8ff1c5b039d945b
korges <korgesik@gmail.com>
2026-01-01 13:36:42 +0100

    testInvariantBreaksWhenPriceIsZero

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -387,12 +387,32 @@ contract DSCEngine is ReentrancyGuard {
         return ADDITIONAL_FEED_PRECISION;
     }
 
+    function getLiquidationThreshold() external pure returns (uint256) {
+        return LIQUIDATION_THRESHOLD;
+    }
+
     function getLiquidationBonus() external pure returns (uint256) {
         return LIQUIDATION_BONUS;
     }
 
     function getLiquidationPrecision() external pure returns (uint256) {
         return LIQUIDATION_PRECISION;
+    }
+
+    function getMinHealthFactor() external pure returns (uint256) {
+        return MIN_HEALTH_FACTOR;
+    }
+
+    function getCollateralTokens() external view returns (address[] memory) {
+        return s_collateralTokens;
+    }
+
+    function getDsc() external view returns (address) {
+        return address(i_dsc);
+    }
+
+    function getCollateralTokenPriceFeed(address token) external view returns (address) {
+        return s_priceFeeds[token];
     }
 
     function getHealthFactor(address user) external view returns (uint256) {
